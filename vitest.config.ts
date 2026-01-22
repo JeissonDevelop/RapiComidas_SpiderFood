314c1c47bb2857ee1a00a81c78c8b3c499546319
JeissonDevelop <jei.gomez15@gmail.com>
2026-01-22 19:34:31 +0100

    AC. 5.5 - Deploy

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,9 @@
 import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
+  plugins: [react()],
+  base: "/RapiComidas_SpiderFood/",
   test: {
     environment: "jsdom",
     setupFiles: ["./src/__tests__/setup.ts"],
